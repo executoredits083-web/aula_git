6e26c90166c2a80881a9cbae09828748510fe578
MARCUS VINICIUS CORDEIRO DE SOUZA <marcus.cordeiro@aluno.senac.rio>
2026-03-11 21:23:24 -0300

    atualizando o prog5.js

--- prog5.js
+++ prog5.js
@@ -1,1 +1,13 @@
 console.log('Ola mundo, testando a exportação de um arquivo criado no Github para o VS Code');
+let custo = 1000;
+switch(custo){
+    case 1000:
+        console.log('Comum');
+        break;
+    case 500:
+        console.log('Em promoção');
+        break;
+    default:
+        console.log('Não há unidades');
+        break;
+}
